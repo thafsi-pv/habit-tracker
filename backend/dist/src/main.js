31db40bf68fbc6ac7b1e11cb8657c6b08bd07267
Thafsi PV <pvthafsi@gmail.com>
2026-09-02 18:21:03 +0530

    Add local build artifacts for backend and frontend

---
+++ backend/dist/src/main.js
@@ -0,0 +1,33 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const common_1 = require("@nestjs/common");
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const app_module_1 = require("./app.module");
+const http_exception_filter_1 = require("./common/filters/http-exception.filter");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, {
+        logger: ['error', 'warn', 'log'],
+    });
+    const appUrl = process.env.APP_URL ?? 'http://localhost:5173';
+    app.enableCors({
+        origin: appUrl,
+        credentials: true,
+    });
+    app.use((0, cookie_parser_1.default)());
+    app.useGlobalPipes(new common_1.ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+        transformOptions: { enableImplicitConversion: true },
+    }));
+    app.useGlobalFilters(new http_exception_filter_1.HttpExceptionFilter());
+    const port = process.env.PORT ?? 3000;
+    await app.listen(port);
+    console.log(`API listening on http://localhost:${port}`);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
